678bb12fa562a01916808731a98d6440d605aa85
LeMalparido <hans.arteaga93@gmail.com>
2019-10-15 16:43:01 -0500

    just added the GUI logic, removed the csv-to-json line

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-const csv2json = require('csvtojson');
+const button = document.getElementById('queryButton')
+button.addEventListener('click', ()=> {
+  
+  const input = document.getElementById('emailValue').value 
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  var found = input.match(regex)
+  
+  console.log(found)
+})
 
-const csv = `email
-email1@test.com
-email2@dominio.com
-usuario3@dominio.com
-johndoe@dominio.com`
-
-const csvArray= []
+const textAreaArray= []
 const xmlArray= []
 
-const json = csv2json()
-.fromString(csv)
-.then((csvRow)=>{ 
-    csvArray.push(csvRow)
-    console.log(csvArray[0])
-    csvArray[0].map(({email}) => {
-        const exclusionRuleTemplate = `
-      <exclude>
-        <match>ADDRESS</match>
-        <type>EXACT</type>
-        <filter>${email}</filter>
-      </exclude>`
-        xmlArray.push(exclusionRuleTemplate)
-    })
-    xmlArray.map((x)=>console.log(x))
-})
+function handleChange(e){
+  console.log(e.target.value) 
+} 
 
