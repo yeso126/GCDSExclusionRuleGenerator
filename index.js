bc7f81c6e9f0ae59a17c40eb6957939f71af2a32
Yesid Ghisays <yeso126@hotmail.com>
2019-10-15 23:27:21 -0500

    -Corrected regular expression for Emails -CSS styles addition

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const button = document.getElementById('queryButton')
-button.addEventListener('click', ()=> {
-  
-  const input = document.getElementById('emailValue').value 
-  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  var found = input.match(regex)
-  
-  console.log(found)
+
+button.addEventListener('click', () => {
+	const txtAreaInput = document.getElementById('emailValue').value
+	const regex = new RegExp(/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/g)
+	const foundEmails = txtAreaInput.match(regex)
+  console.log(foundEmails)
+  // TODO
+  // Map array to execute function to follow a template string and push each template into a new array, finally display the array as a value for the outputbox.
 })
-
-const textAreaArray= []
-const xmlArray= []
-
-function handleChange(e){
-  console.log(e.target.value) 
-} 
-
